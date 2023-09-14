23ee3cdc07da2eb33fe12a3d06691796f4e6f23d
Joey <jyudel@gmail.com>
2023-09-14 14:51:42 -0400

    removing type check testing

--- main/src/main.rs
+++ main/src/main.rs
@@ -6,19 +6,9 @@ use dfdx::prelude::*;
 use rust_games_games::Othello;
 use rust_games_main::Leaderboard;
 use rust_games_players::{AlphaZero, Corners, Greedy, Random};
-use rust_games_shared::{Game, Strategy};
-
-type Inp<G: Game> = Tensor<(Const<{ G::CHANNELS }>, G::BoardSize, G::BoardSize), f32, AutoDevice>;
-//dfdx::tensor::Trace<f32, AutoDevice>>::Traced
-type Test<G> = BoardGameModel<G>;
-
-type Built<G> = <Test<G> as BuildOnDevice<AutoDevice, f32>>::Built;
-
-type Out<G> = <Built<G> as ModuleMut<<Inp<G> as Trace<f32, AutoDevice>>::Traced>>::Output;
+use rust_games_shared::Strategy;
 
 fn main() {
-    print!("{}", std::any::type_name::<Out<Othello>>());
-    assert!(false);
     let corner_player = Strategy::new("Corners".to_string(), Corners::new());
     let rand_player_1 = Strategy::new("Random1".to_string(), Random::<Othello>::new());
     let rand_player_2: Strategy<Othello> = Strategy::new("Random2".to_string(), Random::new());
@@ -28,17 +18,17 @@ fn main() {
 
     let dumb_az_player = Strategy::new(
         "Dumb Alphazero".to_string(),
-        AlphaZero::new_from_file::<BoardGameModel<Othello>>("control", 1.0, &dev, false),
+        AlphaZero::new_from_file::<BoardGameModel<Othello>>("control", 1.0, &dev, false, 100),
     );
 
     let az_player_best = Strategy::new(
         "AlphaZero Best".to_string(),
-        AlphaZero::new_from_file::<BoardGameModel<Othello>>("best", 1.0, &dev, false),
+        AlphaZero::new_from_file::<BoardGameModel<Othello>>("best", 1.0, &dev, false, 100),
     );
 
     let az_player_latest = Strategy::new(
         "AlphaZero latest".to_string(),
-        AlphaZero::new_from_file::<BoardGameModel<Othello>>("latest", 1.0, &dev, false),
+        AlphaZero::new_from_file::<BoardGameModel<Othello>>("latest", 1.0, &dev, false, 100),
     );
 
     let players = vec![
